14a255ad05f898f1881e73b60bd5e35f46cd8bce
Carly Langlois <cj.langlois.cl@gmail.com>
2017-05-03 16:49:56 -0500

    unit test added to EditPopup

--- __tests__/components/EditPopup.test.js
+++ __tests__/components/EditPopup.test.js
@@ -110,4 +110,26 @@ describe('EditPopup', function() {
     ReactDOM.unmountComponentAtNode(container);
   });
 
+  it('correct set of layers is taken into consideration', function() {
+    var container = document.createElement('div');
+    var layer = new ol.layer.Tile({
+      name: 'x',
+      visible: false,
+      source: new ol.source.TileWMS({url: 'http://foo', params: {LAYERS: 'y'}})
+    });
+    map.addLayer(layer);
+    var popup = ReactDOM.render((
+      <EditPopup intl={intl} map={map} />
+    ), container);
+    var layers = popup._getLayers();
+    assert.equal(layers.length, 0);
+    layer.set('popupInfo', '[foo]');
+    layers = popup._getLayers();
+    assert.equal(layers.length, 0);
+    layer.setVisible(true);
+    layers = popup._getLayers();
+    assert.equal(layers.length, 1);
+    ReactDOM.unmountComponentAtNode(container);
+  });
+
 });
