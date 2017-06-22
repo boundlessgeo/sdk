cf58f2cf3685c244639bf7f116de90282102e55d
Carly Langlois <cj.langlois.cl@gmail.com>
2017-06-22 13:12:27 -0500

    deleting map action

--- src/components/Map.js
+++ src/components/Map.js
@@ -25,8 +25,6 @@ const mapStateToProps = (state) => {
 // Maps actions to props
 const mapDispatchToProps = (dispatch) => {
   return {
-    getMap: map => dispatch(MapActions.getMap(map)),
-    //getProjection: map => dispatch(MapActions.getProjection(map)),
     setSize: (size) => dispatch(MapActions.setSize(size)),
     setView: (view) => dispatch(MapActions.setView(view)),
     clearLayerDrag: () => dispatch(LayerActions.clearDrag())
