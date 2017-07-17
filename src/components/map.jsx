a905d3a9350772bfcb4b0a8ded373a68c4872966
Willie Nordmann <wnordmann@boundlessgeo.com>
2017-07-17 13:18:35 -0500

    Building new example
    Fixing build for featretable appFixing build for featretable appFixing build for featretable appFixing build for featretable appFixing build for featretable appFixing build for featretable appFixing build for featretable appFixing build for featretable appFixing build for featretable app
    
    starting base
    
    added index.html to apps.
    created a _src to house working code to create a base to make this app deployable.
    
    Base - boilerplate added
    
    added partials for example app
    
    added files to git ignore
    
    updated git ignore
    
    Adding in css
    
    added readme and sample styles
    
    fixed to get the app to work..
    
    Building out sample app
    
    created webpack config added
    watch task to package.json to test the dist generation
    
    finished the build for app
    
    Dynamically adding in data
    
    adjusted asset to be more flexable.
    correctly processed scss into one file
    
    updated sdk.css to sdk.scss
    
    Working with loading in features
    
    updated webpack config to output CSS correctly
    
    swapped sdk and app reference
    
    Adding in dynamic polygon addition
    
    removed sample app
    
    finished removing app
    
    Adding in some style
    
    Adding in data file
    
    fixed config
    
    Adjusting styleing on popup
    
    added doctype
    
    Adding popup.css
    
    Adding stylesheet partial
    
    turned the css into scss
    
    Cleaning up the feature Table example and adding comments
    
    Update .gitignore
    
    removed a file that is not supposed to be ignored
    
    Cleaning up commented code
    
    Working with crs closer
    
    Working with crs closer
    
    Squash
    
    Fixing epsg check
    
    Working with crs
    
    Cleaning comments and adjusting .json
    
    Cleaning up commented code
    
    Working with crs closer
    
    Squash
    
    Fixing epsg check
    
    Working with crs
    
    Added support for the layer 'ref' property. (#564)
    
    1. Added logic to the map component to recognize
       the 'ref' attribute.
    2. Added a test to ensure coverage.
    
    Added the ability to tweak the map's metadata.
    
    1. Added new updateMetadata action.
    2. Added reducer code to handle the update action.
    
    Added some dangle
    
    Added support for detecting changes in layer styles (#569)
    
    1. Added support for detecting changes in a layers
       style.
    2. Created a demo of changing the paint settings.
    3. Removed the continue from the configure layers loop.
        Added layer !== null to the ref-type loop.
    
    configureLayers handles changes to min/max zoom vals (#567)
    
    Added support for min/maxzoom on layers.
    
    Added an example of labels on clusters
    
    This label example would also work for other
    layer types but is demo'd on the clustered
    layers.
    
    Lint clean up + added comment about stops
    
    Cleaning comments and adjusting .json
    
    Cleaning merge errors
    
    Cleaning out old folders
    
    Cleaning up scss
    
    Adding in comments

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -60,6 +60,7 @@ const WGS84_SPHERE = new Sphere(6378137);
 /** This variant of getVersion differs as it allows
  *  for undefined values to be returned.
  */
+
 function getVersion(obj, key) {
   if (obj.metadata === undefined) {
     return undefined;
@@ -122,10 +123,20 @@ function configureMvtSource(glSource) {
 }
 
 
-function updateGeojsonSource(olSource, glSource) {
+function updateGeojsonSource(olSource, glSource, mapProjection) {
   // parse the new features,
-  // TODO: This should really check the map for the correct projection.
-  const features = GEOJSON_FORMAT.readFeatures(glSource.data, { featureProjection: 'EPSG:3857' });
+
+  let features;
+  let glSourceCrs = undefined;
+  if(glSource.crs && glSource.crs.properties && glSource.crs.properties.name){
+    glSourceCrs = glSource.crs.properties.name;
+  }
+
+  if(glSource.data.features){
+    const readFeatureOptions = { featureProjection: mapProjection || 'EPSG:3857',
+      dataProjection: glSourceCrs}
+    features = GEOJSON_FORMAT.readFeatures(glSource.data,readFeatureOptions);
+  }
 
   let vector_src = olSource;
 
@@ -138,10 +149,14 @@ function updateGeojsonSource(olSource, glSource) {
       olSource.setDistance(glSource.clusterRadius);
     }
   }
-  // clear the layer WITHOUT dispatching remove events.
-  vector_src.clear(true);
-  // bulk load the feature data.
-  vector_src.addFeatures(features);
+
+  if(features){
+    // clear the layer WITHOUT dispatching remove events.
+    vector_src.clear(true);
+    // bulk load the feature data
+    vector_src.addFeatures(features || null);
+
+  }
 }
 
 /** Create a vector source based on a
@@ -199,6 +214,13 @@ function getResolutionForZoom(map, zoom) {
   const max_rez = view.getMaxResolution();
   return view.constrainResolution(max_rez, zoom - view.getMinZoom());
 }
+
+function getResolutionForZoom(map, zoom) {
+  const view = map.getView();
+  const max_rez = view.getMaxResolution();
+  return view.constrainResolution(max_rez, zoom - view.getMinZoom());
+}
+
 
 export class Map extends React.Component {
 
@@ -267,7 +289,7 @@ export class Map extends React.Component {
 
         if (this.props.map.metadata[version_key] !== nextProps.map.metadata[version_key]) {
           const next_src = nextProps.map.sources[src_name];
-          updateGeojsonSource(this.sources[src_name], next_src);
+          updateGeojsonSource(this.sources[src_name], next_src, this.map.getView().getProjection().getCode());
         }
       }
     }
