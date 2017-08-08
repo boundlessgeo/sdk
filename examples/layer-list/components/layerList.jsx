f3e8e0fe5a949daff42a9eddfd99722b28d89def
Jeremy Mulenex <jeremy@aboutjm.com>
2017-08-08 13:24:35 -0500

    moved checkbox and adjusted styles

--- examples/layer-list/components/layerList.jsx
+++ examples/layer-list/components/layerList.jsx
@@ -31,7 +31,7 @@ class layerList extends React.PureComponent {
         <button className="sdk-btn" onClick={() => { this.props.moveLayer(layer.id, layers[i - 1].id); }}>Move down</button>
       </span>);
 
-      list.push(<li  className="layer" key={i}><span className="name">{layer.id}</span><span className="checkbox">{checkbox}</span><span className="btn-container">{moveButtons}</span></li>);
+      list.push(<li  className="layer" key={i}><span className="checkbox">{checkbox}</span> <span className="name">{layer.id}</span><span className="btn-container">{moveButtons}</span></li>);
     }
     return (<ul className="sdk-layerlist">{list}</ul>);
   }
